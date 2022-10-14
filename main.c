7587b6ad9f9641defc6533cdc905456654d91cf4
Karol Szychulski <karol.szychulski@snow.dog>
2022-10-14 23:23:14 +0200

    basic linked list nothing special

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+
+struct Node
+{
+    void* e_chain;
+    struct Node* e_next;
+    /* data */
+};
+
+int spawnNode(void* element)
+{
+    struct Node* head = NULL;
+    head = (struct Node*)malloc(sizeof(struct Node));
+
+    &ptr.e_chain = element;
+
+}
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
